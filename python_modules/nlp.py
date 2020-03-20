a2ca5332c4073a90e7e7a962b437a1be42e25ea4
Kevin Chen <Kevin@Chaldea.local>
2020-03-19 20:51:05 -0700

    Updated some docstrings

--- python_modules/nlp.py
+++ python_modules/nlp.py
@@ -7,12 +7,22 @@ from google.cloud.language import types
 
 class NLP:
     def __init__(self):
+        """ Uses Google Cloud NLP libraries to calculate sentiments.
+        """
         # Sets environmental varable
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = f"{pathlib.Path(__file__).parent.resolve()}/google_cloud_api.json"
         # Instantiates a client
         self.client = language.LanguageServiceClient()
 
     def analyze(self, text):
+        """ Given a text string calculate the sentiment.
+        
+        Args:
+            text (str): text to test the sentiment
+        
+        Returns:
+            dict: a sentiment dictionary with the score and the magnitude.
+        """
         document = types.Document(
             content=text,
             type=enums.Document.Type.PLAIN_TEXT,
